396972d2021521e52b2a799e8e802ab003cfea50
Joey <2504657518@qq.com>
2022-08-21 02:45:29 +0800

    feat:完成城市选择器-省市区联动

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -2,8 +2,9 @@
   <div class="el-select-container">
     <!-- 省 -->
     <el-select
-      v-model="provinceValue"
-      placeholder="请选择省份">
+      v-model="province"
+      placeholder="请选择省份"
+      clearable>
       <el-option
         v-for="item in allAreas"
         :key="item.code"
@@ -13,9 +14,10 @@
     </el-select>
     <!-- 市 -->
     <el-select
-      v-model="cityValue"
+      v-model="city"
       placeholder="请选择城市"
-      :disabled="!provinceValue">
+      :disabled="!province"
+      clearable>
       <el-option
         v-for="item in selectCities"
         :key="item.code"
@@ -25,9 +27,10 @@
     </el-select>
     <!-- 区 -->
     <el-select
-      v-model="areaValue"
+      v-model="area"
       placeholder="请选择区域"
-      :disabled="!provinceValue || !cityValue">
+      :disabled="!province || !city"
+      clearable>
       <el-option
         v-for="item in selectAreas"
         :key="item.code"
@@ -39,27 +42,73 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import allAreasArr from '../lib/pca-code.json'
 
-// 选中数据
-const provinceValue = ref<string>('')
-const cityValue = ref<string>('')
-const areaValue = ref<string>('')
+export interface IAreaItem {
+  name: string,
+  code: string,
+  children?: IAreaItem[]
+}
+export interface IData {
+  name: string,
+  code: string
+}
+
+// 选中code
+const province = ref<string>('')
+const city = ref<string>('')
+const area = ref<string>('')
 
 // 所有省市区数据
 const allAreas = ref(allAreasArr)
-console.log('所有省市区数据', allAreas.value);
 // 城市下拉框数据
-let selectCities = computed(() => {
-  if(!provinceValue) {
-    return []
-  } else {
+let selectCities = ref<IAreaItem[]>([])
+// 区域下拉框数据
+let selectAreas = ref<IAreaItem[]>([])
+
+// 为父组件分发事件(省/市/区都有值时)
+let emits = defineEmits(['change'])
+
+// 监听省份
+watch(() => province.value, val => {
+  city.value = ''
+  area.value = ''
+  if (val) {
+    let cities = allAreas.value.find(item => item.code === province.value).children
+    selectCities.value = cities
   }
 })
-// console.log('城市下拉框数据', selectCities);
-// 区域下拉框数据
-let selectAreas = computed(() => {
+// 监听城市
+watch(() => city.value, val => {
+  area.value = ''
+  if (val) {
+    let areas = selectCities.value.find(item => item.code === city.value).children
+    selectAreas.value = areas
+  }
+})
+// 监听区域
+watch(() => area.value, val => {
+  // 构造省/市/区数据
+  let provinceData: IData = {
+    name: province.value && allAreas.value.find(item => item.code === province.value).name,
+    code: province.value
+  }
+  let cityData: IData = {
+    name: city.value && selectCities.value.find(item => item.code === city.value).name,
+    code: city.value
+  }
+  let areaData: IData = {
+    name: val && selectAreas.value.find(item => item.code === val).name,
+    code: val
+  }
+  if (val) {
+    emits('change', {
+      province: provinceData,
+      city: cityData,
+      area: areaData
+    })
+  }
 })
 </script>
 
--- src/router/views/chooseArea/index.vue
+++ src/router/views/chooseArea/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <j3-choose-area></j3-choose-area>
+    <j3-choose-area @change="changeArea"></j3-choose-area>
   </div>
 </template>
 
 <script lang="ts" setup>
 // import j3ChooseArea from '../../../components/chooseArea/src/index.vue'
+let changeArea = val => {
+  console.log(val)
+}
 </script>
 
 <style lang="scss" scoped>
